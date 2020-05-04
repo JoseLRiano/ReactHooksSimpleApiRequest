0ec85e1d96999e7e6f8ce7a89f1381277934fff9
JoseLRiano <joselriano@yahoo.com>
2020-05-04 03:19:38 -0400

    Updates

---
+++ precache-manifest.7b16f54caed6491b3c58e5697d61c982.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d78ef9d7664c8359c9893a1507237d6",
+    "url": "/ReactHooksSimpleApiRequest/index.html"
+  },
+  {
+    "revision": "129cc71e96571d26e182",
+    "url": "/ReactHooksSimpleApiRequest/static/js/2.5c502bf6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactHooksSimpleApiRequest/static/js/2.5c502bf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61fc82de00f55c20d62a",
+    "url": "/ReactHooksSimpleApiRequest/static/js/main.ac22e603.chunk.js"
+  },
+  {
+    "revision": "8cbfb1d8cc2f80fba2b0",
+    "url": "/ReactHooksSimpleApiRequest/static/js/runtime-main.8e0234cd.js"
+  }
+]);
